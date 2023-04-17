b9164890f6a4ba94f9eb47c9787dd389d8bd5d29
urjakhadilkar <61882870+urjakhadilkar@users.noreply.github.com>
2023-04-17 11:55:17 -0500

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,70 +1,21 @@
-# A Python server that takes submissions, runs them through a pretrained model, and sends
-# them to the front end through a websocket.
-from flask import Flask, render_template, request, abort
+from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
-# import json
-# import sys
+model = None
+app = Flask(__name__)
+CORS(app)
 
-# model = None
-# app = Flask(__name__)
-# CORS(app)
-
-# # # Send the frontend the list of backend models to choose from
-# # @app.route('/modeltypes', methods=['GET', 'POST'])
-# # @cross_origin()
-# # def getModelTypes():
-# #   return json.dumps({'modeltypes': ["sample","layout transformer","gpt"]})
-
-# @app.route('/imagesuggestions', methods=['POST'])
-# @cross_origin()
-# def getSuggestions(): # Takes objects in format [name, box]
-#   j = request.get_json()
-
-#   # labels, boxes = model.auto(labels, boxes,screenTypes)
-#   print(json.dumps({"boxes": boxes, "labels": labels}))
-#   return json.dumps({"boxes": boxes, "labels": labels})
-
-# @app.route('/textsuggestions', methods=['POST'])
-# @cross_origin()
-# def getSuggestions(): # Takes objects in format [name, box]
-#     j = request.get_json()
-#     # print(j)
-#     if 'screenTypes' in j:
-#         screenTypes = f"A well-designed {j['screenTypes']} screen"
-#     else:
-#         screenTypes = "A well-designed UI screen"
+@app.route('/api/complete_text', methods=['POST'])
+@cross_origin()
+def complete_text():
     
-#     if 'modelTypes' in j:
-#         modelToBeUsed = j['modelTypes']
-#     else:
-#         modelToBeUsed = "gpt"
+    return jsonify({'message': 'Endpoint 1 called'})
+
+@app.route('/api/get_image', methods=['POST'])
+@cross_origin()
+def get_image():
     
-#     labels, boxes= j['labels'], j['boxes'] 
-#     try:
-#         if modelToBeUsed == "sample":
-#             labels, boxes = model_sample.auto(labels, boxes,screenTypes)
-#         elif modelToBeUsed == "layout transformer":
-#             labels, boxes = model_lt.auto(labels, boxes,screenTypes)
-#         else:
-#             labels, boxes = model_gpt.auto(labels, boxes,screenTypes)
-#     except:
-#         abort(500, description="Internal Server Error")
-   
-#     print(json.dumps({"boxes": boxes, "labels": labels}))
-#     return json.dumps({"boxes": boxes, "labels": labels})
-# # python server.py
-# if __name__ == '__main__':
-#   try:
-#     # module = __import__('models.')  
-#     # print(module)
-#     model_sample = sample()
-#     model_lt = lt()
-#     model_gpt = gpt()
-#     # model = getattr(module, sys.argv[1]).AutoCompleteModel()
-#     app.run(host="0.0.0.0",debug=True, port=2020)
-#     # app.run(host="0.0.0.0",debug=True)
-#   except Exception as e:
-#     print(e)
-#     print("Usage is:\t'python server.py <model_script_name>'")
-#     print("Example:\t'python server.py autocomplete_sample'")
+    return jsonify({'message': 'Endpoint 2 called'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
