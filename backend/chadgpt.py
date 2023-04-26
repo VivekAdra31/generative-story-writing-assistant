3abeed1507687ad6f90c25ed64a48f303c464f4f
Arijit Ghosh Chowdhury <arijit10@gmail.com>
2023-04-25 23:58:27 -0500

    Update chadgpt.py

--- backend/chadgpt.py
+++ backend/chadgpt.py
@@ -4,9 +4,10 @@ import os
 openai.api_key = os.environ['OPENAI_API_KEY']
 
 def return_response(prompt):
+  system_prompt = " You are writing a children's story book. Complete the following text by adding 200 words to it :  "
   completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": prompt}]
+    messages=[{"role": "user", "content": system_prompt + prompt}]
   )
 
   context = completion['choices'][0]["message"]["content"]
